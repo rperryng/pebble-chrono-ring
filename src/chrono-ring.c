e413863e52288223520ba94ea73dfc0d587ee6bc
Ryan Perry-Nguyen <ryanp@pebble.com>
2016-02-16 15:48:36 -0800

    show hour text

--- src/chrono-ring.c
+++ src/chrono-ring.c
@@ -1,5 +1,65 @@
 #include <pebble.h>
 
+static Window *s_main_window;
+static TextLayer *s_hour_layer;
+
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  static char s_buffer[sizeof("00")];
+  strftime(
+      s_buffer,
+      sizeof(s_buffer),
+      clock_is_24h_style() ? "%H" : "%I",
+      tick_time
+  );
+  text_layer_set_text(s_hour_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(s_main_window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_hour_layer = text_layer_create(GRect(0, 58, bounds.size.w, 50));
+  text_layer_set_background_color(s_hour_layer, GColorClear);
+  text_layer_set_text_color(s_hour_layer, GColorBlack);
+  text_layer_set_font(s_hour_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_hour_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_hour_layer, "00:00");
+
+  update_time();
+
+  layer_add_child(window_layer, text_layer_get_layer(s_hour_layer));
+}
+
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_hour_layer);
+}
+
+static void init() {
+  s_main_window = window_create();
+
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
 int main(void) {
+  init();
   app_event_loop();
+  deinit();
 }
